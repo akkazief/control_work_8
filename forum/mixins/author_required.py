50c0bd48002892fd666b35bf8379c25e631438df
akkaziev_aktan <salizardfnite@gmail.com>
2026-07-04 14:08:59 +0600

    added author_required mixin. Instantly added possibility for moderator

---
+++ forum/mixins/author_required.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class AuthorRequiredMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.author or self.request.user.groups.filter(name='Moderator').exists()
