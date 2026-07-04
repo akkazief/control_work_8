94746b7b1f1ba46c8962110da0c4693c836a2efb
akkaziev_aktan <salizardfnite@gmail.com>
2026-07-04 15:30:42 +0600

    from single list.py made directory which branches to base model and list_of_topics and profiles

---
+++ forum/views/topics/list/base_list.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.db.models import Count
+from forum.models import Topic
+
+
+class BaseTopicListView(ListView):
+    model = Topic
+    context_object_name = "topics"
+    paginate_by = 5
+    paginate_orphans = 2
+
+    def get_base_queryset(self):
+        return Topic.objects.select_related('author').annotate(
+            replies_count=Count('replies')
+        ).order_by('-created_at')
---
+++ forum/views/topics/list/topics_list.py
@@ -0,0 +1,7 @@
+from .base_list import BaseTopicListView
+
+class TopicListView(BaseTopicListView):
+    template_name = "topics/list.html"
+
+    def get_queryset(self):
+        return self.get_base_queryset()
